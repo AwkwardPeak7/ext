dab399026bb4ac08f500139714b9952f2cae2ac6
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-06-21 10:04:53 +0500

    madara

--- lib-multisrc/madara/build.gradle.kts
+++ lib-multisrc/madara/build.gradle.kts
@@ -1,12 +1,15 @@
 import keiyoushi.gradle.extensions.baseVersionCode
+import keiyoushi.gradle.extensions.libVersion
 
 plugins {
     alias(kei.plugins.multisrc)
 }
 
 baseVersionCode = 51
+libVersion = "1.6"
 
 dependencies {
-    api(project(":lib:cryptoaes"))
-    api(project(":lib:i18n"))
+    // api(project(":lib:cryptoaes"))
+    // api(project(":lib:i18n"))
+    api(project(":lib:cookieinterceptor"))
 }
